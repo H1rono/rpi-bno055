03cd1c80f9a8bb95b8a535f0dbbdab4c0f42aef7
H1rono <hronok66@gmail.com>
2024-07-05 15:54:04 +0900

    Add data_files

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup()
+package_name = "rpi_bno055"
+
+setup(
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ["package.xml"]),
+    ]
+)
